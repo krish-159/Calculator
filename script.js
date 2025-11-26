303d0cad548ef4fdde85f31babdcfc6a63e7659b
krish-159 <mymothersulo@gmail.com>
2025-11-27 03:33:41 +0530

    Update prompts for number input in script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let n1 = parseFloat(prompt("Enter no."));
+let n1 = parseFloat(prompt("Enter no.1"));
 while (Number.isNaN(n1)) {
-    n1 = parseFloat(prompt("Invalid no. Enter no. again"));
+    n1 = parseFloat(prompt("Invalid no. Enter no.1 again"));
 }
 
-let n2 = parseFloat(prompt("Enter no."));
+let n2 = parseFloat(prompt("Enter no.2"));
 while (Number.isNaN(n2)) {
-    n2 = parseFloat(prompt("Invalid no. Enter no. again"));
+    n2 = parseFloat(prompt("Invalid no. Enter no.2 again"));
 }
 let operator = prompt("Enter operator (+, -, *, /)");
 let result;
